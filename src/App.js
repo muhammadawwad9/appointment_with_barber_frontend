b3d2f0034a8f38664cda922e03ab4cdd4d4b5687
halaassaly <hala.assaly@gmail.com>
2020-12-31 13:25:36 +0200

    we added the menu bar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
 import Inputs from "./components/Public/Inputs";
 import Title from "./components/Public/Title";
@@ -9,20 +10,23 @@ import Buttons from "./components/Public/Buttons";
 import Card from "./components/Public/Card";
 
 function App() {
+  const [state, setState] = React.useState(false);
   const menuClick = () => {
     console.log("clicked!");
+    setState(!state);
+    console.log(state);
   };
   return (
     <div className="App">
-      <h1>App component</h1>
-      {/* <Menu />
       <Navbar username="Nuwrss" menuClick={menuClick} />
+      {state == true ? <Menu menuClick={menuClick} /> : null}
+
       <Title title="LOG IN" />
-      <Inputs icon="img/profile-user.svg" type="email" placeholder="email" /> */}
-      {/* <h1>App component</h1>
+      <Inputs icon="img/profile-user.svg" type="email" placeholder="email" />
+
       <Buttons text="Login" />
       <Card businessName="cut pro" location="waze://tamra" avg="4.3" />
-     <Card businessName="perfect cut" location="waze://Kafr-Qara" avg="4.14" />*/}
+      <Card businessName="perfect cut" location="waze://Kafr-Qara" avg="4.14" />
     </div>
   );
 }
--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -2,7 +2,23 @@ import React from "react";
 import "./style.css";
 
 const Menu = (props) => {
-  return <div className="menu"></div>;
+  return (
+    <div className="menu">
+      <div className="topmenusection">
+        {" "}
+        <img
+          src="img/menu.svg"
+          className="menubaricon"
+          onClick={props.menuClick}
+        ></img>
+      </div>
+      <div className="menuComponent">Profile</div>
+      <div className="menuComponent">Favorite Shops</div>
+      <div className="menuComponent">My Appointments</div>
+      <div className="menuComponent">My Businesses</div>
+      <div className="menuComponent">Logout</div>
+    </div>
+  );
 };
 
 export default Menu;
