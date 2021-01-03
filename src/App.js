6d0c505bf0b9d426ecad104ae85a91d51a1cae60
mj majadly <majadly@nevada.unr.edu>
2021-01-03 12:07:08 +0200

    logout

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import logo from "./logo.svg";
-import React, { useState, useEffect } from "react";
-import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
-import { toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import "./App.css";
+import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import './App.css';
 //components imports
-import Inputs from "./components/Public/Inputs";
-import Title from "./components/Public/Title";
-import Navbar from "./components/Navbar/Navbar";
-import Menu from "./components/Menu/Menu";
-import Buttons from "./components/Public/Buttons";
-import Card from "./components/Public/Card";
-import Login from "./components/Login/Login";
-import Businesses from "./components/Businesses/Businesses";
+import Inputs from './components/Public/Inputs';
+import Title from './components/Public/Title';
+import Navbar from './components/Navbar/Navbar';
+import Menu from './components/Menu/Menu';
+import Buttons from './components/Public/Buttons';
+import Card from './components/Public/Card';
+import Login from './components/Login/Login';
+import Businesses from './components/Businesses/Businesses';
 toast.configure();
 
 function App() {
@@ -21,38 +21,48 @@ function App() {
   const [user, setUser] = useState({});
 
   const menuClick = () => {
-    console.log("clicked!");
+    console.log('clicked!');
     setState(!state);
     console.log(state);
   };
 
-  console.log("STATE IS:  ", isLoggedIn);
+  console.log('STATE IS:  ', isLoggedIn);
 
   //useEffect, checking the local storage to see if the user is logged in
   useEffect(() => {
-    const access_token = window.localStorage.getItem("access_token");
+    const access_token = window.localStorage.getItem('access_token');
     if (access_token) setIsLoggedIn(true);
     else setIsLoggedIn(false);
   }, [isLoggedIn]);
   return (
     <BrowserRouter>
-      <Navbar isLoggedIn={isLoggedIn} username="Nuwrss" menuClick={menuClick} />
+      <Navbar
+        isLoggedIn={isLoggedIn}
+        setIsLoggedIn={setIsLoggedIn}
+        username='Nuwrss'
+        menuClick={menuClick}
+      />
       {state == true ? (
-        <Menu isLoggedIn={isLoggedIn} menuClick={menuClick} />
+        <Menu
+          isLoggedIn={isLoggedIn}
+          setIsLoggedIn={setIsLoggedIn}
+          menuClick={menuClick}
+          setState={setState}
+        />
       ) : null}
-      <div className="App">
+      <div className='App'>
         <Switch>
-          <Route exact path="/">
-            <Redirect to="/login" />
+          <Route exact path='/'>
+            <Redirect to='/login' />
           </Route>
-          <Route exact path="/login">
+          <Route exact path='/login'>
             {isLoggedIn ? (
-              <Redirect to="/getbusiness" />
+              <Redirect to='/getbusiness' />
             ) : (
               <Login isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
             )}
           </Route>
-          <Route exact path="/getbusiness">
+          <Route exact path='/getbusiness'>
             <Businesses />
           </Route>
           <Navbar />
--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,31 +1,70 @@
-import React from "react";
-import "./style.css";
+import React, { useState, useEffect } from 'react';
+import { NavLink, withRouter } from 'react-router-dom';
+import './style.css';
 
 const Menu = (props) => {
+  const logOut = () => {
+    localStorage.removeItem('access_token');
+    // setUser({});
+    props.setState(false);
+    props.setIsLoggedIn(false);
+  };
+  const hide = () => {
+    props.setState(false);
+  };
+
   return (
-    <div className="menu">
-      <div className="topmenusection">
-        {" "}
+    <div className='menu'>
+      <div className='topmenusection'>
+        {' '}
         <img
-          src="img/menu.svg"
-          className="menubaricon"
+          src='img/menu.svg'
+          className='menubaricon'
           onClick={props.menuClick}
         ></img>
       </div>
       {props.isLoggedIn ? (
-        <div>
-          <div className="menuComponent">Profile</div>
-          <div className="menuComponent">Favorite Shops</div>
-          <div className="menuComponent">My Appointments</div>
-          <div className="menuComponent">My Businesses</div>
-          <div className="menuComponent">Logout</div>
-        </div>
+        <ul>
+          <li className='menuComponent'>
+            <NavLink to='/' onClick={hide}>
+              Profile
+            </NavLink>
+          </li>
+          <li className='menuComponent'>
+            <NavLink to='/' onClick={hide}>
+              Favorite Shops
+            </NavLink>
+          </li>
+          <li className='menuComponent'>
+            <NavLink to='/' onClick={hide}>
+              My Appointments
+            </NavLink>
+          </li>
+          <li className='menuComponent'>
+            <NavLink to='/' onClick={hide}>
+              My Businesses
+            </NavLink>
+          </li>
+          <li className='menuComponent'>
+            {' '}
+            <NavLink to='/login' onClick={logOut}>
+              Log Out
+            </NavLink>
+          </li>
+        </ul>
       ) : (
-        <div>
-          {" "}
-          <div className="menuComponent">Login</div>
-          <div className="menuComponent"> Signup</div>
-        </div>
+        <ul>
+          <li className='menuComponent'>
+            <NavLink to='/' onClick={hide}>
+              Login
+            </NavLink>
+          </li>
+          <li className='menuComponent'>
+            <NavLink to='/' onClick={hide}>
+              Signup
+            </NavLink>
+          </li>{' '}
+        </ul>
       )}
     </div>
   );
